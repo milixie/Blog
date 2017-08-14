fe8aaaf1992096ddf4698e71fb340deff4742afb
milixie <xiemeili@5wei.com>
2017-08-14 18:21:56 +0800

    add

---
+++ webpack/src/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import _ from 'lodash';
+import printMe from './print.js';
+import './styles.css';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  })
+}
---
+++ webpack/src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+  console.log('updating print.js !');
+}
+
