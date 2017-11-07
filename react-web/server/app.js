3c26b43cb9037bcbade4a05f52001109d6782f6c
milixie <xiemeili@5wei.com>
2017-11-07 20:30:55 +0800

    update

--- react-web/server/app.js
+++ react-web/server/app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+// app.set("view options", {layout: false});
+const engines = require('consolidate');
+
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+  res.render('../index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Express listen 3000');
+});
