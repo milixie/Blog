9346a7c28291081640b5846a1986cd3a30e78ef1
xiemeili <xiemeili@luojilab.com>
2018-07-18 15:48:12 +0800

    add webpack to build files

---
+++ react/web/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <p>This is react part test </p>
+  )
+}
+
+export default App
---
+++ react/web/webpack.dev.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const root = __dirname
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  // 入口文件
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client',
+    'webpack/hot/only-dev-server',
+    path.resolve(root, 'src/main.js')
+  ],
+  // 出口文件
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(root, 'dist'),
+    publicPath: '/'
+  },
+  // loaders
+  module: {
+    rules: [
+      {test: /\.jsx?$/, use: ['babel-loader'], exclude: /node_modules/}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Demo',
+      template: path.resolve(root, 'template.html')
+    }),
+    new webpack.HotModuleReplacementPlugin(),  // 热替换插件
+    new webpack.NamedModulesPlugin()  // 执行热替换时打印模块名字
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(root, 'dist'),
+    publicPath: '/',
+    port: 8080,
+    historyApiFallback: true
+  }
+}
